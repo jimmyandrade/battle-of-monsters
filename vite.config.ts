d2ab03010a7e675923d7a1c9cf1bb9194fa956f2
Jimmy Andrade <contato@jimmyandrade.com>
2022-10-13 20:08:28 -0300

    style: Format code with ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
 
 /**
  * @see https://vitejs.dev/config/
@@ -8,8 +8,7 @@ export default defineConfig({
   plugins: [viteReact()],
   server: {
     headers: {
-      "Content-Security-Policy": "connect-src 'self' http://localhost ws://localhost:*; default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' https://via.placeholder.com; manifest-src 'self'; script-src-elem 'self' 'unsafe-inline'; style-src 'self' https://fonts.googleapis.com; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com"
+      'Content-Security-Policy': "connect-src 'self' http://localhost ws://localhost:*; default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' https://via.placeholder.com; manifest-src 'self'; script-src-elem 'self' 'unsafe-inline'; style-src 'self' https://fonts.googleapis.com; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com"
     }
   }
-});
-
+})
